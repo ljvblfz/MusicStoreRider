0625375922cdafeb613a6b659e8eeb369a2838e7
ljvblfz <ljvblfz@users.noreply.github.com>
2017-02-08 02:27:59 +0800

    Remove unused directives in solution

--- MusicStore/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MusicStore/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,8 +2,6 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using MusicStore.Data;
 
 namespace MusicStore.Data.Migrations
 {
